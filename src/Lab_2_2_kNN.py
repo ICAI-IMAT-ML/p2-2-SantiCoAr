a8bbc00a73ed3e9eb802353093a07b753afec8fd
SantiCoAr <202313967@alu.comillas.edu>
2025-02-08 16:24:27 +0100

    Pruebas en notebook

--- src/Lab_2_2_kNN.py
+++ src/Lab_2_2_kNN.py
@@ -421,7 +421,7 @@ def plot_roc_curve(y_true, y_probs, positive_label):
     y_true_mapped = np.array([1 if label == positive_label else 0 for label in y_true])
 
     # Ensure all possible thresholds are included
-    thresholds = np.linspace(0, 1, len(y_probs) + 1)
+    thresholds = np.linspace(0, 1, 11)
 
     fpr = []
     tpr = []
